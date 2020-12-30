a3066955482242f4b9bdc8e4f53d8fd24acb8c76
abdulezzat <41158260+abdulezzat@users.noreply.github.com>
2020-12-30 22:47:13 +0200

    Update Node.cc

--- Distributed/src/Node.cc
+++ Distributed/src/Node.cc
@@ -63,7 +63,8 @@ void Node::sendData()
     mmsg->setSeq_Num(next_frame_to_send);
     mmsg->setM_Type(FrameArrival);
     mmsg->setAck((frame_expected+MAX_SEQ)%(MAX_SEQ+1)); // Ack contains the number of the last frame received.
-    send(mmsg, "outs", getDest()); // to physical layer
+    //send(mmsg, "outs", getDest()); // to physical layer
+    toPhysicalLayer(mmsg);
     //TODO: start timer.
 }
 
@@ -127,6 +128,139 @@ void Node::startGoBackN(MyMessage_Base* msg)
     } else{
         EV<<"Network layer of Node"<<getIndex() <<" is disabled"<<endl;
     }
+
+}
+
+/*
+ * this function make a frame using byte stuffing with :
+ *
+ */
+
+MyMessage_Base Node::frame(char *msg ){
+    MyMessage_Base mmsg("") ;
+    std::string payLoad = "";
+    const char* flag = par("Flag").stringValue();
+    const char* ESC = par("ESC").stringValue();
+    for(int i = 0;i<strlen(msg);i++){
+        if(msg[i] == *flag || msg[i] == *ESC){
+            payLoad.push_back(par("ESC"));
+            payLoad.push_back(msg[i]);
+        }else{
+            payLoad.push_back(msg[i]);
+        }
+    }
+    payLoad = "$"+payLoad+"$";
+
+    char* c_payLoad = new char[payLoad.size()];
+    std::strcpy(c_payLoad, payLoad.c_str());
+    mmsg.setM_Payload(c_payLoad);
+    return mmsg;
+}
+
+/*
+ * this function make a frame using byte stuffing with :
+ *
+ */
+char* Node::unframe(MyMessage_Base mmsg){
+   std::string msg = "";
+   const char* received_msg = mmsg.getM_Payload();
+   const char* ESC = par("ESC").stringValue();
+   for(int i = 1 ; i < strlen(received_msg) - 1; i++){
+       if(received_msg[i] == *ESC){
+           msg.push_back(received_msg[++i]);
+       }else{
+           msg.push_back(received_msg[i]);
+       }
+   }
+
+   char* c_msg = new char[msg.size()];
+   std::strcpy(c_msg, msg.c_str());
+
+   return c_msg;
+}
+//channel handling methods
+/*
+ * this function is used to send the message
+ */
+void Node::toPhysicalLayer(MyMessage_Base* msg){
+    bool lost = lossEffect();
+    if(!lost){
+        duplicateEffect(msg);
+        modificationEffect(msg);
+        delaysEffect(msg);
+    }
+}
+/*
+ * this function is used to simulate the modification of a bit per frame
+ */
+void Node::modificationEffect(MyMessage_Base* msg){
+    int rand = uniform(par("modification_min").doubleValue(),par("modification_max").doubleValue())*100;
+    if(par("modification_presentage").intValue()>=rand){
+        int position_bit = uniform(0,(strlen(msg->getM_Payload())*8) - 1);
+        int position_char = position_bit / 8;
+        const char * old_msg = msg->getM_Payload();
+        int n = strlen(old_msg);
+        char* m_msg =new char[n];
+        std::strcpy(m_msg, old_msg);
+
+        std::bitset<8> bits(m_msg[position_char]);
+        bits[position_bit] = ~bits[position_bit];
+
+        m_msg[position_char] = *(bits.to_string().c_str());
+        msg->setM_Payload(m_msg);
+        EV<<"frame is modified at bit position : "<< position_bit <<endl;
+        EV<<"frame is modified at char position : "<< position_char <<endl;
+        EV<<old_msg[position_char]<< " --> " <<m_msg[position_char]<<endl;
+
+    }
+
+
+
+}
+/*
+ * this function is used to simulate the duplication effect
+ */
+void Node::duplicateEffect(MyMessage_Base* msg){
+    int rand = uniform(par("duplication_min").doubleValue(),par("duplication_max").doubleValue())*100;
+    if(par("duplication_presentage").intValue()>=rand){
+       EV<<"frame is duplicated !!!"<<endl;
+       const char * old_msg = msg->getM_Payload();
+       int n = 2*strlen(old_msg);
+       char* d_msg =new char[n];
+       for(int i = 0 ; i < n;i++){
+           d_msg[i] = old_msg[i%(n/2)];
+       }
+       msg->setM_Payload(d_msg);
+    }
+}
+/*
+ * this function is used to simulate the loss effect of the message
+ * if function returned true, then the message will be lost and won't be received
+ * if false then the message will be received
+ */
+bool Node::lossEffect(){
+    bool result = false;
+    int rand = uniform(par("loss_min").doubleValue(),par("loss_max").doubleValue())*100;
+    if(par("loss_presentage").intValue()>=rand){
+       result = true;
+       EV<<"frame is lost !!!"<<endl;
+    }
+    return result;
+}
+/*
+ * this function is used to simulate the delay effect of the channel by sending
+ * the message after a random delay
+ */
+void Node::delaysEffect(MyMessage_Base* msg){
+
+    int rand = uniform(par("delay_chance_min").doubleValue(),par("delay_chance_max").doubleValue())*100;
+    if(par("delay_chance_presentage").intValue()>=rand){
+        EV<<"frame is delayed !!!"<<endl;
+        double rand_time = uniform(par("delay_duration_min").doubleValue(),par("delay_duration_max").doubleValue());
+        sendDelayed(msg,rand_time,"out",getDest());
+     }else{
+        send(msg, "outs", getDest());
+     }
 
 }
 
